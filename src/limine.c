fe8047b0ecb3b8adb2079b4ab159e413f942c475
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-01 13:41:03 +0000

    Limine fixes

--- src/limine.c
+++ src/limine.c
@@ -1,26 +1,43 @@
 #include "limine.h"
+#include <stddef.h>
 
-volatile struct limine_framebuffer_request __attribute__((section(".limine_requests"))) 
-    framebuffer_request = { LIMINE_FRAMEBUFFER_REQUEST, 0, 0 };
-volatile struct limine_memmap_request __attribute__((section(".limine_requests"))) 
-    memmap_request = { LIMINE_MEMMAP_REQUEST, 0, 0 };
-volatile struct limine_hhdm_request __attribute__((section(".limine_requests"))) 
-    hhdm_request = { LIMINE_HHDM_REQUEST, 0, 0 };
-volatile struct limine_rsdp_request __attribute__((section(".limine_requests"))) 
-    rsdp_request = { LIMINE_RSDP_REQUEST, 0, 0 };
-volatile struct limine_kernel_file_request __attribute__((section(".limine_requests"))) 
-    kernel_file_request = { LIMINE_KERNEL_FILE_REQUEST, 0, 0 };
+/* Base revision for all requests */
+volatile LIMINE_BASE_REVISION(3);
 
-__attribute__((section(".limine_requests_start_marker")))
-volatile void *volatile limine_requests_start_marker = NULL;
+/* Request declarations */
+volatile struct limine_framebuffer_request framebuffer_request = {
+    .id = LIMINE_FRAMEBUFFER_REQUEST,
+    .revision = 1
+};
 
-__attribute__((section(".limine_requests_end_marker")))
-volatile void *volatile limine_requests_end_marker = NULL;
+volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST,
+    .revision = 2
+};
 
-__attribute__((section(".limine_kernel_info")))
-struct limine_kernel_info kernel_info = {
+volatile struct limine_hhdm_request hhdm_request = {
+    .id = LIMINE_HHDM_REQUEST,
+    .revision = 1
+};
+
+volatile struct limine_rsdp_request rsdp_request = {
+    .id = LIMINE_RSDP_REQUEST,
+    .revision = 1
+};
+
+volatile struct limine_kernel_file_request kernel_file_request = {
+    .id = LIMINE_KERNEL_FILE_REQUEST,
+    .revision = 1
+};
+
+/* Start/end markers */
+volatile LIMINE_REQUESTS_START_MARKER;
+volatile LIMINE_REQUESTS_END_MARKER;
+
+/* Kernel info structure */
+volatile struct limine_kernel_info kernel_info = {
     .magic = LIMINE_KERNEL_INFO_MAGIC,
-    .revision = 0,
-    .requests_start = &limine_requests_start_marker,
-    .requests_end = &limine_requests_end_marker
+    .revision = 3,
+    .requests_start = (void *)(uintptr_t)&limine_requests_start_marker,
+    .requests_end   = (void *)(uintptr_t)&limine_requests_end_marker
 };
