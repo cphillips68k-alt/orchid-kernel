c7754975e6a37c9de13ea2e9a005a0949401e6fd
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-02 02:39:07 +0000

    tss.c fix

--- src/kernel/tss.c
+++ src/kernel/tss.c
@@ -4,19 +4,31 @@
 
 struct tss {
     uint32_t reserved0;
-    uint64_t rsp0;
-    uint64_t rsp1;
-    uint64_t rsp2;
-    uint64_t reserved1;
-    uint64_t ist1;
-    uint64_t ist2;
-    uint64_t ist3;
-    uint64_t ist4;
-    uint64_t ist5;
-    uint64_t ist6;
-    uint64_t ist7;
-    uint64_t reserved2;
-    uint16_t reserved3;
+    uint32_t rsp0_low;
+    uint32_t rsp0_high;
+    uint32_t rsp1_low;
+    uint32_t rsp1_high;
+    uint32_t rsp2_low;
+    uint32_t rsp2_high;
+    uint32_t reserved1;
+    uint32_t reserved2;
+    uint32_t ist1_low;
+    uint32_t ist1_high;
+    uint32_t ist2_low;
+    uint32_t ist2_high;
+    uint32_t ist3_low;
+    uint32_t ist3_high;
+    uint32_t ist4_low;
+    uint32_t ist4_high;
+    uint32_t ist5_low;
+    uint32_t ist5_high;
+    uint32_t ist6_low;
+    uint32_t ist6_high;
+    uint32_t ist7_low;
+    uint32_t ist7_high;
+    uint32_t reserved3;
+    uint32_t reserved4;
+    uint16_t reserved5;
     uint16_t iopb_offset;
 } __attribute__((packed));
 
@@ -30,14 +42,20 @@ static uint8_t io_bitmap_full[4096] __attribute__((aligned(4096))) = {
 extern void tss_flush(uint16_t sel);
 
 void tss_init(void) {
+    /* Zero the TSS */
     for (size_t i = 0; i < sizeof(tss_data); i++)
         ((volatile uint8_t*)&tss_data)[i] = 0;
+
+    /* Tell the GDT about our TSS */
     gdt_set_tss((uint64_t)&tss_data);
+
+    /* Load the TSS selector (0x28 = 5th GDT entry * 8) */
     tss_flush(0x28);
 }
 
 void tss_set_rsp0(uint64_t rsp0) {
-    tss_data.rsp0 = rsp0;
+    tss_data.rsp0_low  = (uint32_t)(rsp0 & 0xFFFFFFFF);
+    tss_data.rsp0_high = (uint32_t)(rsp0 >> 32);
 }
 
 void tss_set_io_bitmap(int full) {
